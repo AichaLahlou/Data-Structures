d42c29dc4b837c06e70439a5b0fc99c9e6eda224
aicha <lahloua96@gmail.com>
2024-02-21 22:32:09 +0100

    add raise error messages in case the queue is empty

--- src/Queue.py
+++ src/Queue.py
@@ -6,22 +6,26 @@ Created on Tue Feb 20 22:27:56 2024
 """
 
 class Queue:
-  def __init__(self):
-    self.items = []
-    
-  def is_empty(self):
-    return len(self.items)== 0
+    def __init__(self):
+        self.items = []
 
-  def size(self):
-    return len(self.items)
+    def is_empty(self):
+        return len(self.items) == 0
 
-  def add(self, item):
-    self.items.append(item)
-    
-  def remove(self):
-    if not self.is_empty():
-      self.items = self.item.pop(0) 
-      
-  def peek(self):
-    if not self.is_empty():
-      return self.items[0]
+    def size(self):
+        return len(self.items)
+
+    def add(self, item):
+        self.items.append(item)
+
+    def remove(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Queue is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            raise IndexError("Queue is empty")
